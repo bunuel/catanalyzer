8776595044a0c8bf37686dbebb66ab96fe8a084f
bunuel <lenhunt@gmail.com>
2020-08-16 07:23:55 -0400

    started putting in variables for the different variable parts of the text (location, etc)

--- app/src/main/java/com/lenhunt/catanalyzer/MainActivity.kt
+++ app/src/main/java/com/lenhunt/catanalyzer/MainActivity.kt
@@ -11,22 +11,16 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.View
 import android.widget.Toast
-//import com.lenhunt.catanalyzer.R.string
 import com.lenhunt.catanalyzer.R.string.*
+import com.lenhunt.catanalyzer.R.array.locations
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSION_CODE = 1000
-    private val IMAGE_CAPTURE_CODE = 1001
-    var image_uri: Uri? = null
-    //private var output_text = "@string/output_text"
-
-    //var outputText = ""
-
-
+    private val _permissionCode = 1000
+    private val _imageCaptureCode = 1001
+    private var _imageURI: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +37,7 @@ class MainActivity : AppCompatActivity() {
                     //permission was not enabled
                     val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     //show popup to request permission
-                    requestPermissions(permission, PERMISSION_CODE)
+                    requestPermissions(permission, _permissionCode)
                 }
                 else{
                     //permission already granted
@@ -61,18 +55,17 @@ class MainActivity : AppCompatActivity() {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        //camera intent
+        _imageURI = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, _imageURI)
+        startActivityForResult(cameraIntent, _imageCaptureCode)
     }
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         //called when user presses ALLOW or DENY from Permission Request Popup
         when(requestCode){
-            PERMISSION_CODE -> {
+            _permissionCode -> {
                 if (grantResults.size > 0 && grantResults[0] ==
                     PackageManager.PERMISSION_GRANTED){
                     //permission from popup was granted
@@ -90,21 +83,49 @@ class MainActivity : AppCompatActivity() {
         //called when image was captured from camera intent
         if (resultCode == Activity.RESULT_OK){
             //set image captured to image view
+            picture_container.setImageURI(_imageURI)
 
-            picture_container.setImageURI(image_uri)
-            //picture_container.
-            var text_to_output: String = getString(output_text_1).plus(" ").plus(getString(location_text)).plus(getString(output_text_2)).plus(" ").plus(getString(personality_text)).plus(getString(output_text_3)).plus(" ").plus(getString(history))
+            //val values = ContentValues()
+
+            //val randomNumbers: MutableList<Int> = getRandomNumber(3);
+
+            val randomNumber = (0 until 10).random()
+            val locationText: String = getString(locations[(randomNumber)])
+
+            //randomNumber = (0 until 10).random()
+
+           //var location_text: String = getString("location_00".plus(firstRandomNumber))
+
+            //arrayOf(values.[values.[]])
+            var textToOutput: String = getString(output_text_1).plus("\n").plus(locationText).plus("\n").plus(getString(output_text_2)).plus(" ").plus(getString(personality_text)).plus("\n").plus(getString(output_text_3)).plus(" ").plus(getString(history)).plus("\n")
             //button_take_a_picture.visibility = View.INVISIBLE
-            if (BuildConfig.DEBUG == true) {
-                text_to_output = text_to_output.plus(image_uri.toString())
-                text_to_output = text_to_output.plus(picture_container.height.toString())
+            if (BuildConfig.DEBUG) {
+                textToOutput = textToOutput.plus(_imageURI.toString())
+                textToOutput = textToOutput.plus("    ").plus(picture_container.height.toString())
             }
-            picture_container_text.text = text_to_output
+            picture_container_text.text = textToOutput
         }
     }
 
-    //private fun setOutputText(): String = ("@string/)
+    //
+    //private fun getRandomNumber(numberOfTimes: Int) : MutableList<Int> {
+        //val list: MutableList<Int> = ArrayList(numberOfTimes)
 
+        //list = (0 until 10).random()
+        //for (i in 1..numberOfTimes) {
+            // for() {
+            //add(0 until 10).random().ArrayList()
+            //list[i] = (0 until 10).random()
+            //list += (0 until 10).random()
+       //}
 
+        //return list
+        //}
+    //}
 
 }
+
+private operator fun Int.get(i: Int): Int {
+    return i
+}
+
